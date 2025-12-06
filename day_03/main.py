a2b4e0658b0f8da0d6becfa57a81556d91b74692
Bran De Wachter <brandewachter@gmail.com>
2025-12-06 16:17:58 +0100

    solved task 1 & 2

--- day_03/main.py
+++ day_03/main.py
@@ -1,34 +1,59 @@
-high_1 = 0
-high_2 = 0
-sum_of_highest = 0
+with open("day_03/test.txt") as file:
+    banks = [line.strip() for line in file.readlines()]
 
-with open("test.txt") as file:
-    all_lines = []
-    for line in file.readlines():
-        new_line = []
-        # this is how you would loop through each alphabet
-        for chars in line:
-            if chars != "\n":
-                new_line.append(int(chars))
-        all_lines.append(new_line)
+    def findHighIndex(bank: str, start: int, end: int):
+        global digit1_index, digit2_index
+        index = start
+        highestNr = 0
+        highestNrIndex = start
+        while index < end:
+            if (int(bank[index]) > highestNr) and (index != digit1_index):
+                highestNr = int(bank[index])
+                highestNrIndex = index
+            index += 1
+        return highestNrIndex
+    
+    def extractAndConcat(bank: str, firstIndex: int, secondIndex: int):
+        test = int(bank[firstIndex] + bank[secondIndex])
+        return test
+    
+    def remove3tocreatelargestnumber(bank: str):
+        newbank = []
+        toRemove = len(bank) - 12
+        removedCount = 0
+        
+        for digit in bank:
+            while newbank and newbank[len(newbank)-1] < digit and removedCount < toRemove:
+                newbank.pop()
+                removedCount += 1
+            newbank.append(digit)
+        
+        while removedCount < toRemove:
+            newbank.pop()
+            removedCount += 1
 
-    for line in all_lines:
-        print(line)
-        sorted_line = line.sort()
-        print(type(line))
-        print(sorted_line)
+        return ''.join(newbank)
+                
 
-        # for letter in line:
-    print(sorted(all_lines))
+    task1sum = 0
+    task2sum = 0
+    for bank in banks:
+        digit1_index = digit2_index = -1
+        digit1_index = findHighIndex(bank, 0, len(bank)-1)
+        digit2_index = findHighIndex(bank, digit1_index, len(bank))
+        
+        if digit1_index > digit2_index:
+            digit1_index, digit2_index = digit2_index, digit1_index
+    
+        task1result = extractAndConcat(bank, digit1_index, digit2_index)
+        #print("digit1index ",digit1_index)
+        #print("digit2index ",digit2_index)
+        print("result task1: ", task1result)
+        task1sum += task1result
 
-    for battery in all_lines:
-        sum_of_highest += int(str(battery[0]) + str(battery[1]))
+        task2result = remove3tocreatelargestnumber(bank)
+        print("result task2: ", task2result)
+        task2sum += int(task2result)
 
-    print(sum_of_highest)
-
-    #         if(letter > high_1):
-    #             high_1 = letter
-
-
-# print(all_lines)
-
+    print("task1sum: ", task1sum)
+    print("task2sum: ", task2sum)
